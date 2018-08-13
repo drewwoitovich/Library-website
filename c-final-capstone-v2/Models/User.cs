0ea2d255cd044dac7409f9a9b8fbf1cb99b730c2
Drew Woitovich <drew.woitovich@live.com>
2018-08-13 11:57:19 -0400

    added user model and updated sql txt file

---
+++ c-final-capstone-v2/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace c_final_capstone_v2.Models
+{
+    public class User
+    {
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime LastSearch { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool Newsletter { get; set; }
+        public string Email { get; set; }
+    }
+}
