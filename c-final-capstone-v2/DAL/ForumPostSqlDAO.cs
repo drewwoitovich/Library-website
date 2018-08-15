ba6dc71faffd294dc6ef10ff1d2a6e0903bc266e
Drew Woitovich <drew.woitovich@live.com>
2018-08-15 15:09:27 -0400

    beginning work on forum while we wait for sprint review. added a forumpost model and wrote a dao method to return a list of all forum posts

---
+++ c-final-capstone-v2/DAL/ForumPostSqlDAO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using c_final_capstone_v2.Models;
+using System.Data.SqlClient;
+
+namespace c_final_capstone_v2.DAL
+{
+    public class ForumPostSqlDAO
+    {
+
+        private string connectionString;
+        // Constructor
+        public ForumPostSqlDAO(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        private static string sqlGetAllForumPosts = "SELECT [username], [message] FROM forum";
+           
+        public List<ForumPost> GetAllFoumPosts()
+        {
+            List<ForumPost> allPosts = new List<ForumPost>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand(sqlGetAllForumPosts, conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        ForumPost post = new ForumPost();
+
+                        post.Username = Convert.ToString(reader["username"]);
+                        post.Message = Convert.ToString(reader["message"]);
+
+
+                        allPosts.Add(post);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            return allPosts;
+        }
+    }
+}
