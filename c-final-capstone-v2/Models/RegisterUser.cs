2cb05ee82435b1dd840157f35b97026e477a6b2e
Drew Woitovich <drew.woitovich@live.com>
2018-08-22 13:42:00 -0400

    added validation messages for failing when trying to register users or log in

--- c-final-capstone-v2/Models/RegisterUser.cs
+++ c-final-capstone-v2/Models/RegisterUser.cs
@@ -8,21 +8,21 @@ namespace c_final_capstone_v2.Models
 {
     public class RegisterUser
     {
-        [Required]
+        [Required(ErrorMessage = "Username is required")]
         public string Username { get; set; }
 
         [Required]
         [MinLength(8, ErrorMessage = "Please enter a password that is at least 8 characters.")]
         public string Password { get; set; }
 
-        [Compare("Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
         [Display(Name = "Confirm Password")]
         public string ConfirmPassword { get; set; }
 
         [Display(Name = "Subscribe to our newsletter")]
         public bool Newsletter { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Email is required")]
         [EmailAddress]
         public string Email { get; set; }
     }
