090cb6bf6495a7503eae40f92548fcf36c17a847
Bradon <bstudebaker17@gmail.com>
2018-08-23 11:17:03 -0400

    minor bug fixes

--- c-final-capstone-v2/Scripts/toggle-date.js
+++ c-final-capstone-v2/Scripts/toggle-date.js
@@ -1,10 +1,17 @@
 ﻿$(document).ready(function () {
+    Date.prototype.toDateInputValue = (function () {
+        var local = new Date(this);
+        local.setMinutes(this.getMinutes() - this.getTimezoneOffset());
+        return local.toJSON().slice(0, 10);
+    });
+
     $(DateTimeSearchInput).hide();
     $('#NewSearch').prop('checked', false);
 
     $('#NewSearch').change(function () {
         if ($("#NewSearch").is(":checked")) {
             $(DateTimeSearchInput).show();
+            $(DateTimeSearchInput).val(new Date().toDateInputValue());
         }
         else {
             $(DateTimeSearchInput).hide();
